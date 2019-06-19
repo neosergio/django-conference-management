c1a695f01af65c62de0969052c79a1905a2970bc
Sergio Infante <raulsergio9@gmail.com>
2019-06-19 17:44:02 -0500

    Add conferences models

--- conferences/admin.py
+++ conferences/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Location, Conference, Tag
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'latitude', 'longitude')
+
+
+class ConferenceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('word', 'slug')
+
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Conference, ConferenceAdmin)
+admin.site.register(Tag, TagAdmin)
--- conferences/models.py
+++ conferences/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    word = models.CharField(max_length=100)
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.word
+
+
+class Conference(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
