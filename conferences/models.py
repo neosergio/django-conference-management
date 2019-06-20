8061c089645af38cfe68998ec5ec6bd68de4af03
Sergio Infante <raulsergio9@gmail.com>
2019-06-20 07:51:47 -0500

    Add country to models

--- conferences/models.py
+++ conferences/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Location(models.Model):
     name = models.CharField(max_length=255)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
@@ -29,6 +36,7 @@ class Conference(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True, blank=True)
     tags = models.ManyToManyField(Tag)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
